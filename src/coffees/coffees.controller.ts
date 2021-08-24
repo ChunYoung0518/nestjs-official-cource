a0064a772201e533158684a38467ed4c307b9010
chunyoung0518@gmail.com <chunyoung0518@gmail.com>
2021-08-24 14:13:23 +1000

    using repository to access database

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -33,7 +33,6 @@ export class CoffeesController {
   @Post()
   // @HttpCode(HttpStatus.GONE)
   create(@Body() createCoffeeDto: CreateCoffeeDto) {
-    console.log(createCoffeeDto instanceof CreateCoffeeDto);
     return this.coffeeService.create(createCoffeeDto);
   }
 
