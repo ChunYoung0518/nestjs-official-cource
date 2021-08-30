7e15ef86cab8c857a42a07b907ebaa6c3b49f84f
chunyoung0518@gmail.com <chunyoung0518@gmail.com>
2021-08-30 11:02:26 +1000

    e2e test - POST

--- test/coffee/coffees.e2e-spec.ts
+++ test/coffee/coffees.e2e-spec.ts
@@ -1,9 +1,25 @@
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoffeesModule } from '../../src/coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as request from 'supertest';
+import { CreateCoffeeDto } from '../../src/coffees/dto/create-coffee.dto';
+import arrayContaining = jasmine.arrayContaining;
 
 describe('[Feature] Coffee - /coffees', () => {
+  const coffee = {
+    name: 'Shipwreck Roast',
+    brand: 'Buddy Brew',
+    flavors: ['chocolate', 'vanilla'],
+  };
+
+  // const expectedPartialCoffee = expect.objectContaining({
+  //   ...coffee,
+  //   flavors: expect.arrayContaining(
+  //     coffee.flavors.map((name) => expect.objectContaining({ name })),
+  //   ),
+  // });
+
   let app: INestApplication;
 
   beforeAll(async () => {
@@ -24,10 +40,41 @@ describe('[Feature] Coffee - /coffees', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+        transformOptions: {
+          enableImplicitConversion: true, //do not need Type decorator (in PaginationQueryDto) if this option is used
+        },
+      }),
+    );
     await app.init();
   });
 
-  it.todo('Create [POST /]');
+  it('Create [POST /]', () => {
+    return (
+      request(app.getHttpServer())
+        .post('/coffees')
+        .send(coffee as CreateCoffeeDto)
+        .expect(HttpStatus.CREATED)
+        // .then((body) => {
+        //   expect(body).toEqual(expectedPartialCoffee);
+        // });
+        .then(({ body }) => {
+          const expectedCoffee = expect.objectContaining({
+            ...coffee,
+            flavors: expect.arrayContaining(
+              coffee.flavors.map((name) => expect.objectContaining({ name })),
+            ),
+          });
+          expect(body).toEqual(expectedCoffee);
+        })
+    );
+  });
+
   it.todo('Get all [GET /]');
   it.todo('Get one [GET /:id]');
   it.todo('Update one [PATCH /:id]');
